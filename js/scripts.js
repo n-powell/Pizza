d217670ee580e2f663352ad41d12bbb726ee1393
Nick Powell <nickpowells@gmail.com>
2017-04-07 16:21:34 -0700

    add total calculator and clean up indentation and white space from html, css, and js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Business Logic---------
 
-function Pizza () {
+function Pizza() {
   this.toppings = [];
   this.pizzaSize = "";
   this.pizzaPrice = "";
@@ -8,14 +8,13 @@ function Pizza () {
 
 
 Pizza.prototype.fullPizza = function() {
-  return  "Your " + this.pizzaSize + " pizza with " + [this.toppings] + ". Will be $" + this.pizzaPrice + " dollars. Thank You!";
+  return "Your " + this.pizzaSize + " pizza with " + [this.toppings] + ". Will be $" + this.pizzaPrice + " dollars. Thank You!";
 }
 
 Pizza.prototype.pizzaCalc = function() {
   if (this.pizzaSize === "large") {
-    return  20
-  }
-  else if (this.pizzaSize === "small")
+    return 22
+} else if (this.pizzaSize === "small")
     return 12
 }
 
@@ -23,25 +22,30 @@ Pizza.prototype.pizzaCalc = function() {
 
 //User Logic---------
 
-$(document).ready(function(){
+$(document).ready(function() {
+  var pizzaTotal = []
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
     var newPizza = new Pizza();
-    var inputtedToppings = $("input:checkbox[name=toppings]:checked").each(function(){
+    var inputtedToppings = $("input:checkbox[name=toppings]:checked").each(function() {
       var topping = $(this).val();
       newPizza.toppings.push(topping);
     });
-    var inputtedSize = $("input:radio[name=size]:checked").each(function(){
+    var inputtedSize = $("input:radio[name=size]:checked").each(function() {
       var size = $(this).val();
       newPizza.pizzaSize = size;
     });
 
     newPizza.pizzaPrice = newPizza.pizzaCalc();
+    pizzaTotal.push(newPizza.pizzaPrice)
 
+    var sum = pizzaTotal.reduce((a, b) => a + b, 0);
     $("ol#toppings").append("<li>" + newPizza.toppings + "</li>");
     $("#newSize").append("<li>" + newPizza.pizzaSize + "</li>");
     $("#outputPrice").append("<li> $" + newPizza.pizzaPrice + " dollars</li>")
     $("#description").append("<li>" + newPizza.fullPizza() + "</li>");
+    $("#totalPrice").text(sum);
+    console.log(sum);
 
   });
 });
