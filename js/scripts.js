43bef1a4219130b893b9fb0ea60fe582fbc86094
Nick Powell <nickpowells@gmail.com>
2017-04-07 14:36:28 -0700

    clean up white space in html and javascript. add initial styling.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,25 @@ function Pizza () {
   this.toppings = [];
   this.pizzaSize = "";
   this.pizzaPrice = "";
-
 }
 
+
 Pizza.prototype.fullPizza = function() {
-  return  "Your " + this.pizzaSize + " pizza. With " + [this.toppings] + " . Will be $" + this.pizzaPrice + " dollars. Thank You!";
+  return  "Your " + this.pizzaSize + " pizza with " + [this.toppings] + ". Will be $" + this.pizzaPrice + " dollars. Thank You!";
 }
 
 Pizza.prototype.pizzaCalc = function() {
   if (this.pizzaSize === "large") {
     return  20
   }
-  else
+  else if (this.pizzaSize === "small")
     return 12
 }
 
 
 
 //User Logic---------
+
 $(document).ready(function(){
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
@@ -34,14 +35,13 @@ $(document).ready(function(){
       var size = $(this).val();
       newPizza.pizzaSize = size;
     });
+
     newPizza.pizzaPrice = newPizza.pizzaCalc();
 
     $("ol#toppings").append("<li>" + newPizza.toppings + "</li>");
     $("#newSize").append("<li>" + newPizza.pizzaSize + "</li>");
     $("#outputPrice").append("<li> $" + newPizza.pizzaPrice + " dollars</li>")
     $("#description").append("<li>" + newPizza.fullPizza() + "</li>");
-    console.log(newPizza);
-    console.log(newPizza.pizzaPrice);
 
   });
 });
