08f5ff25ad64aae60faaf8fc8f6425642bc1ba08
Nick Powell <nickpowells@gmail.com>
2017-04-07 10:54:57 -0700

    add size to display of either large or small. Spec 3 complete.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@
 
 function Pizza () {
   this.toppings = [];
+  this.pizzaSize = [];
 
 }
 
 Pizza.prototype.fullPizza = function() {
-  return this.toppings
+  return  "Your" + this.pizzaSize + "pizza. With" + this.toppings + ".Will be";
 }
 
 
@@ -21,19 +22,29 @@ $(document).ready(function(){
     var inputtedToppings = $("input:checkbox[name=toppings]:checked").each(function(){
       var topping = $(this).val();
       newPizza.toppings.push(topping);
-      console.log(newPizza.toppings);
+    });
 
-    var inputtedSize = $("input#size").val();
+    var inputtedSize = $("input:radio[name=size]:checked").each(function(){
+      var size = $(this).val();
+      newPizza.pizzaSize.push(size);
+    });
 
 
 
+
+
+
+
+
+
+    console.log(newPizza.pizzaSize);
+    console.log(newPizza.toppings);
+    console.log(newPizza.fullpizza);
 
 
 
     $("ul#toppings").append("<li>" + newPizza.toppings + "</li>");
-
-
-    });
+    $("#newSize").append("<li>" + newPizza.pizzaSize + "</li>");
 
   });
 
